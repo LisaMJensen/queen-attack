c375bdc8c818d6dd2fc22c755dc4ef88ee75caf2
Na Hyung Choi and Uriel Gonzalez <na@>
2019-07-15 13:31:35 -0700

    add basic UI flow to Main()

--- project.cs
+++ project.cs
@@ -1,8 +1,38 @@
 using System;
 
-class ProjectName
+class QueenAttack
 {
   static void Main()
+  {
+    public static Dictionary<string, int> xCoordinates = new Dictionary<string, int>() { {"A",1}, {"B",2}, {"C",3}, {"D",4}, {"E", 5}, {"F",6}, {"G",7}, {"H",8} };
+
+    Console.WriteLine("Enter the horizontal coordinate for the Queen (A-H): ");
+    string input = Console.ReadLine(); // input = "B"
+    int xCoordInputQueen = xCoordinates[input];  // xCoordInputQueen = 2
+
+    Console.WriteLine("Enter the vertical coordinate for the Queen (1-8): ");
+    int yCoordInputQueen = int.Parse(Console.ReadLine()); // yCoordInputQueen = 7
+
+    Console.WriteLine("Enter the horizontal coordinate for another piece (A-H): ");
+    input = Console.ReadLine();
+    int xCoordInputOtherPiece = xCoordinates[input];
+
+    Console.WriteLine("Enter the vertical coordinate for this piece (1-8): ");
+    int yCoordInputOtherPiece = int.Parse(Console.ReadLine());
+
+    determineAttack();
+
+    if (determineAttack() == true)
+    {
+      Console.WriteLine("The queen attacked!");
+    }
+    else
+    {
+      Console.WriteLine("The queen cannot attack.");
+    }
+  }
+
+  static void determineAttack()
   {
     
   }
