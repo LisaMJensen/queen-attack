ddeea159064f9b9f69f41b6f87e4ee2fccffa375
Na Hyung Choi and Uriel Gonzalez <uriel@>
2019-07-15 13:52:27 -0700

    add backend logic, program complete

--- project.cs
+++ project.cs
@@ -1,24 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class QueenAttack
 {
+  public static int xCoordInputQueen;
+  public static int yCoordInputQueen;
+  public static int xCoordInputOtherPiece;
+  public static int yCoordInputOtherPiece;
+
   static void Main()
   {
-    public static Dictionary<string, int> xCoordinates = new Dictionary<string, int>() { {"A",1}, {"B",2}, {"C",3}, {"D",4}, {"E", 5}, {"F",6}, {"G",7}, {"H",8} };
+    Dictionary<string, int> xCoordinates = new Dictionary<string, int>() { {"A",1}, {"B",2}, {"C",3}, {"D",4}, {"E", 5}, {"F",6}, {"G",7}, {"H",8} };
 
     Console.WriteLine("Enter the horizontal coordinate for the Queen (A-H): ");
     string input = Console.ReadLine(); // input = "B"
-    int xCoordInputQueen = xCoordinates[input];  // xCoordInputQueen = 2
+    xCoordInputQueen = xCoordinates[input];  // xCoordInputQueen = 2
 
     Console.WriteLine("Enter the vertical coordinate for the Queen (1-8): ");
-    int yCoordInputQueen = int.Parse(Console.ReadLine()); // yCoordInputQueen = 7
+    yCoordInputQueen = int.Parse(Console.ReadLine()); // yCoordInputQueen = 7
 
     Console.WriteLine("Enter the horizontal coordinate for another piece (A-H): ");
     input = Console.ReadLine();
-    int xCoordInputOtherPiece = xCoordinates[input];
+    xCoordInputOtherPiece = xCoordinates[input];
 
     Console.WriteLine("Enter the vertical coordinate for this piece (1-8): ");
-    int yCoordInputOtherPiece = int.Parse(Console.ReadLine());
+    yCoordInputOtherPiece = int.Parse(Console.ReadLine());
 
     determineAttack();
 
@@ -32,8 +38,21 @@ class QueenAttack
     }
   }
 
-  static void determineAttack()
+  static bool determineAttack()
   {
-    
+    // Queen can attack pieces in the same row or column
+    if (xCoordInputQueen == xCoordInputOtherPiece || yCoordInputQueen == yCoordInputOtherPiece)
+    {
+      return true;
+    }
+    // Queen can attack pieces that are diagonal from it
+    else if (Math.Abs(xCoordInputQueen - xCoordInputOtherPiece) == Math.Abs(yCoordInputQueen - yCoordInputOtherPiece))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 }
